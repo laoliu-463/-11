fec90c9e6bbda9416a086ebe39f483862a400db8
caojianing <2820843439@qq.com>
2025-11-16 14:11:18 +0800

    vault backup: 2025-11-16 14:11:18

--- 数据结构笔记/数据结构/代码练习/02-链表/CircularLinkList.java
+++ 数据结构笔记/数据结构/代码练习/02-链表/CircularLinkList.java
@@ -12,4 +12,46 @@ public void add(int data){
     node.data=data;
     node.node=this;
 }
+public void delete(int data){
+    CircularLinkList node = this;
+    while(node.node!=null){
+        if(node.node.data==data){
+            node.node=node.node.node;
+            return;
+        }
+        node=node.node;
+    }
+    System.out.println("未找到该节点");
+}
+public void change(int data){
+    CircularLinkList node = this;
+    while(node.node!=null){
+        if(node.node.data==data){
+            node.node.data=data;
+            return;
+        }
+        node=node.node;
+    }
+    System.out.println("未找到该节点");
+}
+public void find(int data){
+    CircularLinkList node = this;
+    while(node.node!=null){
+        if(node.node.data==data){
+            System.out.println("找到该节点");
+            return;
+        }
+        node=node.node;
+    }
+    System.out.println("未找到该节点");
+}
+}
+public static void main(String[] args){
+    CircularLinkList circularLinkList = new CircularLinkList();
+    circularLinkList.add(1);
+    circularLinkList.add(2);
+    circularLinkList.add(3);
+    circularLinkList.add(4);
+    circularLinkList.add(5);
+    circularLinkList.show();
 }
