0a4ff3eb81b7fd478d6a5aad7fc655ecf282f1b0
caojianing <2820843439@qq.com>
2025-11-16 13:20:23 +0800

    vault backup: 2025-11-16 13:20:23

--- 数据结构笔记/数据结构/代码练习/01-顺序表/SqList.java
+++ 数据结构笔记/数据结构/代码练习/01-顺序表/SqList.java
@@ -2,5 +2,10 @@ public class SqList <T>{
     int maxnumber=1000;//顺序表固定大小
     int data[];//存储元素的数组
     int length;//顺序表长度
+public SqList(){
+    //构造函数用于初始化
+    data=new int[maxnumber];
+    length=0;
 
+}
 }
