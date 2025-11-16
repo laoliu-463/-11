49919dd0705da6df45d8ca18f08e0334ec2ee76e
caojianing <2820843439@qq.com>
2025-11-16 14:05:56 +0800

    vault backup: 2025-11-16 14:05:56

--- 数据结构笔记/数据结构/代码练习/02-链表/CircularLinkList.java
+++ 数据结构笔记/数据结构/代码练习/02-链表/CircularLinkList.java
@@ -1,4 +1,15 @@
 public class CircularLinkList {
     //普通单链表
+int data;
+CircularLinkList node;
 
+public CircularLinkList(){
+    this.data=0;
+    this.node=null;
+}
+public void add(int data){
+    CircularLinkList node = new CircularLinkList();
+    node.data=data;
+    node.node=this;
+}
 }
