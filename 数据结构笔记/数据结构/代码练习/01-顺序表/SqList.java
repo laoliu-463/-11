cfb5ef67942b9fecfb001a72750d8eca0a467c60
caojianing <2820843439@qq.com>
2025-11-16 13:36:39 +0800

    vault backup: 2025-11-16 13:36:39

--- 数据结构笔记/数据结构/代码练习/01-顺序表/SqList.java
+++ 数据结构笔记/数据结构/代码练习/01-顺序表/SqList.java
@@ -1,15 +1,27 @@
-public class SqList <T>{
-    int maxnumber=1000;//顺序表固定大小
+public class SqList <T> {
+    int maxnumber = 1000;//顺序表固定大小
     int data[];//存储元素的数组
     int length;//顺序表长度
-public SqList(){
-    //构造函数用于初始化
-    data=new int[maxnumber];
-    length=0;
 
+    public SqList() {
+        //构造函数用于初始化
+        data = new int[maxnumber];
+        length = 0;
+
+    }
+    public boolean insert(int i,T x){
+        if(i<1||i>length+1){
+            return false;
+        }
+        if(length>maxnumber){
+            return false;
+        }
+        for(int j=length,j>=length-1,j--){
+
+        }
+
+        return false;
+    }
 }
 
-}
-public static void main(String[] args){
-    SqList<Integer> list=new SqList<>();
-}
+
