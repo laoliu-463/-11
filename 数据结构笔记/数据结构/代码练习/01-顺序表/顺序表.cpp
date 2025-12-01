0fc76d04955e00d909e1403e17af99afaf79de58
caojianing <2820843439@qq.com>
2025-12-01 15:32:08 +0800

    vault backup: 2025-12-01 15:32:08

--- 数据结构笔记/数据结构/代码练习/01-顺序表/顺序表.cpp
+++ 数据结构笔记/数据结构/代码练习/01-顺序表/顺序表.cpp
@@ -6,22 +6,26 @@
 #define MAXNUMBER 100
  typedef struct sqlist
 
-{//int data[MAXNUMBER];
+{int data[MAXNUMBER];
 
-int *data;//指针开动态数组
+//int *data;//指针开动态数组
     int number;
 
 }myarrylist;
  myarrylist initlist() {
     myarrylist a;
-    a.data=(int *)malloc(MAXNUMBER*sizeof(int));
+
     if (a.data == NULL) {
         printf("申请失败");
+        //分配的数组里会有脏数据
     }
+     a.number=0;
+     return a;
 }
 
 
 int main() {
 myarrylist a;
+
 
 }
